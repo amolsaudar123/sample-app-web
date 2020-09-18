e54349909c6e9bf5a333dfe2ae301d07e72b72d2
amolsaudarDoc <amol.saudar@docplexus.co.in>
2020-09-18 23:20:40 +0530

    make it better

--- Local/webpack.config.dev.js
+++ Local/webpack.config.dev.js
@@ -68,14 +68,14 @@ module.exports = {
         cert: './localhost.crt',
         key: './localhost.key',
         host: '0.0.0.0',
-        port: 9999,
+        port: 80,
         hot: true,
         overlay: true,
         historyApiFallback: false,
         watchContentBase: true,
         disableHostCheck: true,
         headers: {
-            'Access-Control-Allow-Origin': https ? 'https://0.0.0.0:9999' : 'http://0.0.0.0:9999'
+            'Access-Control-Allow-Origin': https ? 'https://0.0.0.0:80' : 'http://0.0.0.0:80'
         }
     },
     mode: 'development',
